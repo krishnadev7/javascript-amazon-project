4daebcc26b0edbf79a4d24c09b2ae3e6e3ed9f7d
krishnadev7 <krishnadevv12@gmail.com>
2023-09-29 19:37:13 +0530

    added modules and refactor the code

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,30 @@
-const cart = [];
+export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingItem = item;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
+
+export function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((item) => {
+    cartQuantity += item.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import {addToCart, cart, updateCartQuantity} from '../data/cart.js';
+import { products } from '../data/products.js';
+
 let productHtml = "";
 
 products.forEach((product) => {
@@ -59,23 +62,7 @@ document.querySelector(".js-product-grid").innerHTML = productHtml;
 document.querySelectorAll(".js-addToCart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-    console.log(cart);
+    addToCart(productId)
+    updateCartQuantity();
   });
 });
